6753e113d5dd05067342d999b1ae959dc69a1dd9
Pierre Kisters <pierrekisters@gmail.com>
2020-07-13 23:34:57 +0200

    added CONNECT_TIMEOUT

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ Simple http proxy that delays the response until an expected status code is rece
 - SERVER_HOST: listen host (default: 0.0.0.0)
 - SERVER_PORT: listen port (default: 8080)
 - STATUS_CODES: expected status codes comma separated (default: 200)
-- CLIENT_TIMEOUT: time to delay response until gateway timeout (default: 5min)
-- CLIENT_INTERVAL: retry interval (default: 5s)
+- RETRY_TIMEOUT: time to delay response until gateway timeout (default: 5min)
+- RETRY_INTERVAL: retry interval (default: 5s)
+- CONNECT_TIMEOUT: gateway connect timeout (default: RETRY_INTERVAL)
 
 ## Docker builds
 https://hub.docker.com/repository/docker/lolhens/http-wait
--- src/main/scala/de/lolhens/httpwait/Main.scala
+++ src/main/scala/de/lolhens/httpwait/Main.scala
@@ -1,5 +1,7 @@
 package de.lolhens.httpwait
 
+import java.net.http.HttpClient
+
 import cats.data.OptionT
 import cats.effect.{ExitCode, Resource}
 import cats.syntax.option._
@@ -15,8 +17,9 @@ import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.slf4j.LoggerFactory
 
-import scala.concurrent.duration.{Duration, FiniteDuration}
+import scala.concurrent.duration._
 import scala.jdk.CollectionConverters._
+import scala.util.chaining._
 
 object Main extends TaskApp {
   private val logger = LoggerFactory.getLogger(Main.getClass)
@@ -37,15 +40,17 @@ object Main extends TaskApp {
                      host: String,
                      port: Int,
                      statusCodes: List[Status],
-                     timeout: Duration,
-                     interval: FiniteDuration) {
+                     retryTimeout: Duration,
+                     retryInterval: FiniteDuration,
+                     connectTimeout: FiniteDuration) {
     def debug: String = {
       s"""LOG_LEVEL: $logLevel
          |SERVER_HOST: $host
          |SERVER_PORT: $port
          |STATUS_CODES: ${statusCodes.map(_.code).mkString(",")}
-         |CLIENT_TIMEOUT: $timeout
-         |CLIENT_INTERVAL: $interval""".stripMargin
+         |RETRY_TIMEOUT: $retryTimeout
+         |RETRY_INTERVAL: $retryInterval
+         |CONNECT_TIMEOUT: $connectTimeout""".stripMargin
     }
   }
 
@@ -53,24 +58,39 @@ object Main extends TaskApp {
     def fromEnv: Options = {
       val env: Map[String, String] = System.getenv().asScala.toMap.map(e => (e._1, e._2.trim)).filter(_._2.nonEmpty)
 
+      def requireFinite(duration: Duration, name: String): FiniteDuration = duration match {
+        case finite: FiniteDuration => finite
+        case _ => throw new IllegalArgumentException(s"$name must be finite!")
+      }
+
+      val logLevel: Level = env.get("LOG_LEVEL").map(Level.valueOf).getOrElse(Level.INFO)
+      val host: String = env.getOrElse("SERVER_HOST", "0.0.0.0")
+      val port: Int = env.get("SERVER_PORT").map(_.toInt).getOrElse(8080)
+      val statusCodes: List[Status] = env.get("STATUS_CODES")
+        .map(_.split("\\s*,\\s*").toList.filter(_.nonEmpty).map(e => Status.fromInt(e.toInt).toTry.get))
+        .getOrElse(List(Status.Ok))
+      val retryTimeout: Duration = env.get("RETRY_TIMEOUT").orElse(env.get("CLIENT_TIMEOUT"))
+        .map(Duration(_)).getOrElse(5.minutes)
+      val retryInterval: FiniteDuration = env.get("RETRY_INTERVAL").orElse(env.get("CLIENT_INTERVAL"))
+        .map(Duration(_).pipe(requireFinite(_, "RETRY_INTERVAL"))).getOrElse(5.seconds)
+      val connectTimeout: FiniteDuration = env.get("CONNECT_TIMEOUT")
+        .map(Duration(_).pipe(requireFinite(_, "CONNECT_TIMEOUT"))).getOrElse(retryInterval)
+
       Options(
-        logLevel = Level.valueOf(env.getOrElse("LOG_LEVEL", "INFO")),
-        host = env.getOrElse("SERVER_HOST", "0.0.0.0"),
-        port = env.getOrElse("SERVER_PORT", "8080").toInt,
-        statusCodes = env.getOrElse("STATUS_CODES", "200")
-          .split("\\s*,\\s*").toList.filter(_.nonEmpty).map(e => Status.fromInt(e.toInt).toTry.get),
-        timeout = Duration(env.getOrElse("CLIENT_TIMEOUT", "5min")),
-        interval = Duration(env.getOrElse("CLIENT_INTERVAL", "5s")) match {
-          case finite: FiniteDuration => finite
-          case _ => throw new IllegalArgumentException("CLIENT_INTERVAL must be finite!")
-        }
+        logLevel = logLevel,
+        host = host,
+        port = port,
+        statusCodes = statusCodes,
+        retryTimeout = retryTimeout,
+        retryInterval = retryInterval,
+        connectTimeout = connectTimeout
       )
     }
   }
 
   class Server(options: Options) {
-    lazy val run: Task[Nothing] = Task.deferAction { implicit scheduler =>
-      BlazeServerBuilder[Task]
+    lazy val run: Task[Nothing] = Task.deferAction { scheduler =>
+      BlazeServerBuilder[Task](scheduler)
         .bindHttp(options.port, options.host)
         .withHttpApp(service.orNotFound)
         .resource
@@ -78,7 +98,7 @@ object Main extends TaskApp {
     }
 
     lazy val gatewayTimeout: Task[Response[Task]] = {
-      (options.timeout match {
+      (options.retryTimeout match {
         case finite: FiniteDuration =>
           Task.sleep(finite)
 
@@ -89,7 +109,11 @@ object Main extends TaskApp {
     }
 
     lazy val clientResource: Resource[Task, Client[Task]] =
-      Resource.liftF(JdkHttpClient.simple[Task].memoizeOnSuccess)
+      Resource.liftF(Task(JdkHttpClient[Task](
+        HttpClient.newBuilder()
+          .connectTimeout(java.time.Duration.ofMillis(options.connectTimeout.toMillis))
+          .build()
+      )).memoizeOnSuccess)
 
     lazy val service: HttpRoutes[Task] = HttpRoutes[Task] { request =>
       val requestTime = System.currentTimeMillis()
@@ -133,7 +157,7 @@ object Main extends TaskApp {
                       Task.now(response)
 
                     case None =>
-                      Task.sleep(options.interval) *>
+                      Task.sleep(options.retryInterval) *>
                         retryForCode
                   }
               }
